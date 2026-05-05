fdd8e7844f6dc5c02b5b638615dec959755467bd
Lucas Fontinele <contato@fontinele.dev>
2026-05-05 17:04:07 -0300

    test: refactoring tests and including in-memory repositories

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,23 +1,24 @@
-import type { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
-import { expect, test } from 'vitest'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-const fakeAnswersRepository: AnswersRepository = {
-  async create() {
-    // Simulate saving the answer to a database
-  },
-}
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let answerQuestion: AnswerQuestionUseCase
 
-test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
-
-  const answer = await answerQuestion.execute({
-    instructorId: 'instructor-1',
-    questionId: 'question-1',
-    content: 'This is an answer to the question.',
+describe('Answer question', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    answerQuestion = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
 
-  expect(answer.content).toEqual('This is an answer to the question.')
-  expect(answer.authorId.toString()).toEqual('instructor-1')
-  expect(answer.questionId.toString()).toEqual('question-1')
+  it('should be able to create an answer', async () => {
+    const answer = await answerQuestion.execute({
+      instructorId: 'instructor-1',
+      questionId: 'question-1',
+      content: 'This is an answer to the question.',
+    })
+
+    expect(answer.content).toEqual('This is an answer to the question.')
+    expect(answer.authorId.toString()).toEqual('instructor-1')
+    expect(answer.questionId.toString()).toEqual('question-1')
+  })
 })
--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,24 +1,25 @@
-import { expect, test } from 'vitest'
-import type { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
+import { InMemoryQuestionsRepository } from './../../../../../test/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 
-const fakeQuestionsRepository: QuestionsRepository = {
-  async create() {
-    // Simulate saving the question to a database
-  },
-}
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let createQuestion: CreateQuestionUseCase
 
-test('create a question', async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
-
-  const { question } = await createQuestion.execute({
-    authorId: 'author-1',
-    title: 'This is a question',
-    content: 'This is the content of the question.',
+describe('Create question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    createQuestion = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
-  expect(question.title).toEqual('This is a question')
-  expect(question.content).toEqual('This is the content of the question.')
-  expect(question.authorId.toString()).toEqual('author-1')
-  expect(question.id).toBeTruthy()
+  it('should be able to create a question', async () => {
+    const { question } = await createQuestion.execute({
+      authorId: 'author-1',
+      title: 'This is a question',
+      content: 'This is the content of the question.',
+    })
+
+    expect(question.title).toEqual('This is a question')
+    expect(question.content).toEqual('This is the content of the question.')
+    expect(question.authorId.toString()).toEqual('author-1')
+    expect(question.id).toBeTruthy()
+  })
 })
